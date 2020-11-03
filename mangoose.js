2cb9bc12a66ad7ff2e92a701dce343b7e47e0e04
TimonHC <timofeyyevtushenko@gmail.com>
2020-11-03 13:48:50 +0200

    fixup! refactored .js file added styles

--- mangoose.js
+++ mangoose.js
@@ -1,78 +1,5 @@
 function main() {
 
-    /*function backup_gavnakod{ const menuButton = document.querySelectorAll('.menu-button');
-        const mediaWrapper = document.querySelectorAll('#media-wrapper');
-        const descriptionWrapper = document.querySelectorAll('#description-wrapper');
-        const mediaMain = document.getElementById('media-main');
-        const mediaWorkout = document.getElementById('media-workout');
-        const mediaForparents = document.getElementById('media-for-parents');
-        const mediaForpupils = document.getElementById('media-for-pupils');
-
-        const descriptionMain = document.getElementById('description-main');
-        const descriptionWorkout = document.getElementById('description-workout');
-        const descriptionForparents = document.getElementById('description-for-parents');
-        const descriptionForpupils = document.getElementById('description-for-pupils');
-        const promoVideo = document.getElementById('promo-video');
-
-        mediaWorkout.classList.add('hidden');
-        mediaForparents.classList.add('hidden');
-        mediaForpupils.classList.add('hidden');
-
-        descriptionWorkout.classList.add('hidden');
-        descriptionForparents.classList.add('hidden');
-        descriptionForpupils.classList.add('hidden');
-
-
-        function mainClicked() {
-            mediaMain.classList.remove('hidden');
-            mediaWorkout.classList.add('hidden');
-            mediaForparents.classList.add('hidden');
-            mediaForpupils.classList.add('hidden');
-
-            descriptionMain.classList.remove('hidden');
-            descriptionWorkout.classList.add('hidden');
-            descriptionForparents.classList.add('hidden');
-            descriptionForpupils.classList.add('hidden');
-        }
-
-        function workoutClicked() {
-            mediaMain.classList.add('hidden');
-            mediaWorkout.classList.remove('hidden');
-            mediaForparents.classList.add('hidden');
-            mediaForpupils.classList.add('hidden');
-
-            descriptionMain.classList.add('hidden');
-
-            descriptionWorkout.classList.remove('hidden');
-            descriptionForparents.classList.add('hidden');
-            descriptionForpupils.classList.add('hidden');
-        }
-
-        function forparentsClicked() {
-            mediaMain.classList.add('hidden');
-            mediaWorkout.classList.add('hidden');
-            mediaForparents.classList.remove('hidden');
-            mediaForpupils.classList.add('hidden');
-
-            descriptionMain.classList.add('hidden');
-            descriptionWorkout.classList.add('hidden');
-            descriptionForparents.classList.remove('hidden');
-            descriptionForpupils.classList.add('hidden');
-        }
-
-        function forpupilsClicked() {
-            mediaMain.classList.add('hidden');
-            mediaWorkout.classList.add('hidden');
-            mediaForparents.classList.add('hidden');
-            mediaForpupils.classList.remove('hidden');
-
-            descriptionMain.classList.add('hidden');
-            descriptionWorkout.classList.add('hidden');
-            descriptionForparents.classList.add('hidden');
-            descriptionForpupils.classList.remove('hidden');
-        }
-    }*/
-
 class menuButton {
 
     constructor(name) {
@@ -103,21 +30,14 @@ class menuButton {
 
 const main = new menuButton('main');
 const workout = new menuButton('workout');
-const forParents = new menuButton('for-parents');
-const forPupils = new menuButton('for-pupils');
-menuButtonsManager.menuButtons.push(main, workout, forParents, forPupils);
+const contacts = new menuButton('contacts');
+
+menuButtonsManager.menuButtons.push(main, workout, contacts);
 
 
 
    function toggle(e) {
        menuButtonsManager.hideAnotherButtons(e.target.id);
-           /*switch (e.target.id) {
-           case 'main': ) break;
-           case 'workout': tabList.hideAnotherTabs('workout'); break;
-           case 'for-parents': tabList.hideAnotherTabs('for-parents'); break;
-           case 'for-pupils': tabList.hideAnotherTabs('for-pupils'); break;
-           default: break;
-       }*/
    }
 
 // Проходим по массиву
